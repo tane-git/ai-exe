dee4371b602ed0efab38d48bd399b2a11a11893e
tane-git <tanelives@gmail.com>
2023-06-17 14:40:30 +1200

    WE HAVE REACHED THE SINGULARITY! (got AI to do pwd -> ls -> cat chat.py (which then got blocked by security)

--- chat.py
+++ chat.py
@@ -2,20 +2,27 @@ import os
 import openai
 from dotenv import load_dotenv
 
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+conversation = []
+
 
 def send_message_to_openai(message):
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    conversation.append({"role": "user", "content": message})
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": message},
-        ],
+        # messages=[
+        #     {"role": "system", "content": "You are a helpful assistant."},
+        #     {"role": "user", "content": message},
+        # ],
+        messages=conversation,
     )
 
     response = completion.choices[0].message["content"]
+
+    conversation.append({"role": "system", "content": response})
 
     print("Reponse", response)
 
--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from modules.security import is_command_approved
 from modules.extractor import extract_command
 
 
-if __name__ == "__main__":
-    user_desire = parse_arguments()
-
-    prompt = create_prompt(user_desire)
+def prompting_loop(prompt, depth=0):
+    depth = depth + 1
 
     response = send_message_to_openai(prompt)
 
@@ -17,8 +15,22 @@ if __name__ == "__main__":
 
     if is_command_approved(command):
         stdout, stderr = execute_command(command)
-        print(f"Command output: {stdout}")
+
+        # output is treated as a response from the user
+        if depth < 5:
+            prompting_loop(stdout, depth)
+        else:
+            return
+
         if stderr:
             print(f"Command errors: {stderr}")
     else:
         print(f"Command {response} is not approved for execution.")
+
+
+if __name__ == "__main__":
+    user_desire = parse_arguments()
+
+    prompt = create_prompt(user_desire)
+
+    prompting_loop(prompt)
