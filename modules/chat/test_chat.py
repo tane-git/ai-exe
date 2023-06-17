c04a18cc1e20c8ea9cafad3c418564534e5b9155
tane-git <tanelives@gmail.com>
2023-06-18 11:45:16 +1200

    add test_chat

---
+++ modules/chat/test_chat.py
@@ -0,0 +1,18 @@
+from unittest import mock
+from modules.chat import send_message_to_openai
+
+
+def test_send_message_to_openai():
+    mock_message = mock.MagicMock()
+    mock_message.configure_mock(**{"message": {"content": "Mock response"}})
+
+    mock_choices = mock.MagicMock()
+    mock_choices.configure_mock(**{"__getitem__.return_value": mock_message})
+
+    mock_response = mock.MagicMock()
+    mock_response.configure_mock(**{"choices": mock_choices})
+
+    with mock.patch("openai.ChatCompletion.create", return_value=mock_response):
+        response = send_message_to_openai("Test message")
+
+    assert response == "Mock response"
